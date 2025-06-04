e2623e4fc96509912ac1aff99412c59267a198b4
Shobhit Shourya <157984998+shob0902@users.noreply.github.com>
2025-06-04 17:10:17 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -17,8 +17,8 @@ root = tk.Tk()#Application window
 root.title("Password Generator")
 root.geometry("400x200")
 root.config(bg="#f0f0f0")
-b = tk.Button(root, text="Generate Password", font=("Arial", 14), command=gen_pass)#Button
+b = tk.Button(root, text="🔐Generate Password", font=("Times", 14), command=gen_pass)#Button
 b.pack(pady=20)
-l = tk.Label(root, font=("Arial", 12, "bold"), fg="green", bg="#f0f0f0")#Displaying password
+l = tk.Label(root, font=("Times", 14, "italic"), fg="Blue", bg="#f0f0f0")#Displaying password
 l.pack(pady=20)
 root.mainloop()#Run
